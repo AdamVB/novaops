0c2c862fabd312dc5301818ed2a825d3a0f9cb19
AdamVB <S143504@hft-leipzig.de>
2017-07-14 22:01:00 +0200

    further work

--- novaopsv3.py
+++ novaopsv3.py
@@ -7,13 +7,14 @@ from collections import defaultdict
 import os
 import logging
 import pprint
+import json
 #dont forget to source admin-openrch.sh file into env variables
 
 USERNAME= os.environ['OS_USERNAME']
 PASSWORD = os.environ['OS_PASSWORD']
 AUTH_URL = os.environ['OS_AUTH_URL']
 PROJECT_NAME = os.environ['OS_PROJECT_NAME']
-VERSION = '2' #nova api version
+VERSION = '2.30' #nova api version
 logging.basicConfig(filename='novaops.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 
@@ -25,13 +26,11 @@ nova = client.Client(VERSION, session=sess)
 
 
 
-def GetServers():
-		
+def GetServers():		
 	unsorted_hostlist = []
-	hostlist = defaultdict(list)	
-	
+	hostlist = defaultdict(list)		
 	for server in nova.servers.list(search_opts={'all_tenants': 1}):
-		unsorted_hostlist.append([getattr(server, 'OS-EXT-SRV-ATTR:host', ''),server.id])			
+		unsorted_hostlist.append([getattr(server, 'OS-EXT-SRV-ATTR:host', ''),server.id])	
 	
 	for host, serverid in unsorted_hostlist:
 			hostlist[host].append(serverid)	
@@ -45,25 +44,84 @@ def GetHosts():
 	hostsonlylist = []
 	#print ("Hosts to be touched:")
 	for host in nova.hosts.list():
-		hostsonlylist.append(host)
-		#print(host)		
-	return hostsonlylist
+		if host.service == "compute":
+			hostsonlylist.append(str(host)[7:-1])#cut string so only the actual hostname is saved to list, not <host: nodenamexy>,
+		#print(host)	
 	print("GetHosts Done")
+	return hostsonlylist
 	
+def GetHostsJSON():
+	hostsonlylistJSON = []
+	#print ("Hosts to be touched:")
+	for host in nova.hosts.list():
+		if host.service == "compute":
+			hostsonlylistJSON.append(str(host))
+		#print(host)	
+	print("GetHostsJSON Done")
+	return hostsonlylistJSON
+
+	
+	
+	
+	
+	
+def preflight():
+	writehosts()
+	#loglist = open("hostlist.json","w")
+	print('\n'+"Hosts and Servers to be touched:"+'\n')
+	pprint.pprint(GetServers())	
+	#pprint.pprint(GetHosts(), loglist) # write to file	
+	#with open ("hostlist.json","w") as outfile:
+		#json.dumps(GetHostsJSON(), outfile).encode('utf8')
+	#loglist.close()
+			
+def hostparser():
+	with open("hostlist.txt","r") as f:
+		ops_hostlist = f.readlines()
+		ops_hostlist = [x.strip() for x in ops_hostlist]	
+	#print(*ops_hostlist, sep='\n')
+	return ops_hostlist
+	
+	
+def writehosts():
+	loglist = open("hostlist.txt","w")
+	for host in GetHosts():		
+		loglist.write(host+'\n')
+	loglist.close()
+	
+	
+def migrateVM(vmid):
+	nova.servers.live_migrate( block_migration='auto', server=vmid, host=None)
+	
+def enablehostmaintenance(host):
+	return
+	#enable maintenance mode for host here
+	
+def disablehostmaintenance(host):
+	return
+	#disable maintenance mode for host here
 	
 	
 	
 def ops():
-	loglist = open("hostlist.txt","w")
-	print('\n'+"Hosts and Servers to be touched:"+'\n')
-	pprint.pprint(GetServers())
-	pprint.pprint(GetHosts(), loglist) # write to file
-	loglist.close()
-		
-		
-GetServers()	
-#GetHosts()
+	for host, servers in GetServers().items():
+		if host in hostparser():
+			nova.services.disable(host=host, binary="nova-compute")
+			for s in servers:
+				migrateVM(s)
+			nova.services.enable(host=host, binary="nova-compute")
+		#do migration
+			return
+	
+	
+
+	
+
+
+#preflight()
+#hostparser()
 ops()
+
 
 
 
